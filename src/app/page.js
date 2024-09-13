513809959b786cdacd65b1e271da184b31568b41
Wahib <wahibb07@gmail.com>
2024-09-13 21:43:09 +0500

    nothing much

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,18 @@ import Hero from "@/components/Hero";
 import Preface from "@/components/Preface";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
 export default function Home() {
+  const words = `L o a d i n g . . .`;
+
   const { data: session, status } = useSession();
   console.table(session);
+
   if (status === "loading") {
-    return <h1>Redirecting...</h1>;
+    return <TextGenerateEffect words={words} />;
   }
+
   return (
     <>
       <Navbar out={!session} />
@@ -22,11 +27,11 @@ export default function Home() {
           <CTA />
         </>
       ) : (
-        <div>
-          <h1>Welcome {session.token.name}!</h1>
+        <>
+          <h1>Welcome {session.token.username}!</h1>
           <h1>{session.token.email}</h1>
           <Image alt="user-profile" src={session.token.picture} width={500} height={500} />
-        </div>
+        </>
       )}
     </>
   );
